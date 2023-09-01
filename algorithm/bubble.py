4f50d6f66a4eebf23295d9a8d64fce5ace752c37
Nguyen Cong Duy <ncongduy@gmail.com>
2023-09-01 15:08:39 +0300

    optimize bubble sort algorithm

--- algorithm/bubble.py
+++ algorithm/bubble.py
@@ -2,19 +2,28 @@ def bubble_sort(arr):
     length = len(arr)
 
     for i, it in enumerate(arr):
+        swap = False
         for j, jt in enumerate(arr):
             if j == length - 1:
-                continue
+                break
+
+            print(f'{arr} - {arr[j]} - {arr[j+1]}')
 
             if(arr[j] > arr[j+1]):
-                print(f'{arr[j]} - {arr[j+1]}')
                 temp = arr[j + 1]
                 arr[j + 1] = arr[j]
                 arr[j] = temp
+                swap = True
+            
+        if not swap:
+            break
 
     return arr
 
 
-print(bubble_sort([32, 29, 15, 7, 5, -3, 88, 100]))
+# print(bubble_sort([32, 29, 15, 7]))
+# print(bubble_sort([32, 1, 2, 3]))
+# print(bubble_sort([32, 1, 15, 2, 3]))
+# print(bubble_sort([1, 2, 3, 4, 5]))
 
 # O(n^2)
