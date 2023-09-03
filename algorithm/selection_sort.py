61c49e5be5babf566585d0f4bf938de3b1379aa3
Nguyen Cong Duy <ncongduy@gmail.com>
2023-09-03 17:20:52 +0300

    add selection sort

---
+++ algorithm/selection_sort.py
@@ -0,0 +1,26 @@
+def selection_sort(arr):
+    size = len(arr)
+
+    for i in range(size):
+        min_index = i
+
+        for j in range(i + 1, size):
+            print(f'{arr} - {arr[j]} - {arr[min_index]}')
+
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # swap            
+        temp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index] = temp
+
+    return arr
+
+
+
+print(selection_sort([32, 29, 15, 7]))
+# print(selection_sort([32, 1, 2, 3]))
+# print(selection_sort([32, 1, 15, 2, 3]))
+# print(selection_sort([1, 2, 3, 4, 5]))
+
+## O(n^2)
