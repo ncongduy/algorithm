98fbe815a61549b97112d1c4b1f77827bfd53f0b
Nguyen Cong Duy <ncongduy@gmail.com>
2023-09-10 17:34:57 +0300

    add merge sort algorithm

--- algorithm/merge_sort.py
+++ algorithm/merge_sort.py
@@ -1,31 +1,45 @@
+import math 
+
 def merge_arr(arr1, arr2):
-    new_arr = []
-    length_arr1 = len(arr1)
-    length_arr2 = len(arr2)
-    idx_arr1 = 0
-    idx_arr2 = 0
+    sorted_arr = []
+    i = 0   # index of arr1
+    j = 0   # index of arr2
     min_value = None
 
-    while idx_arr1 < length_arr1 and idx_arr2 < length_arr2:
-        if arr1[idx_arr1] < arr2[idx_arr2]:
-            min_value = arr1[idx_arr1]
-            idx_arr1 += 1
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            min_value = arr1[i]
+            i += 1
         else:
-            min_value = arr2[idx_arr2]
-            idx_arr2 += 1
-        new_arr.append(min_value)
+            min_value = arr2[j]
+            j += 1
+        sorted_arr.append(min_value)
     
-    while idx_arr1 < length_arr1:
-        min_value = arr1[idx_arr1]
-        idx_arr1 += 1
-        new_arr.append(min_value)
+    while i < len(arr1):
+        min_value = arr1[i]
+        i += 1
+        sorted_arr.append(min_value)
 
-    while idx_arr2 < length_arr2:
-        min_value = arr2[idx_arr2]
-        idx_arr2 += 1
-        new_arr.append(min_value)
+    while j < len(arr2):
+        min_value = arr2[j]
+        j += 1
+        sorted_arr.append(min_value)
 
-    return new_arr
+    return sorted_arr
 
-print(merge_arr([1, 10, 50], [2, 14, 99, 100]))
-print(merge_arr([], [1, 3]))
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = math.floor(len(arr)/2)
+    left_arr = merge_sort(arr[slice(0, mid)])
+    right_arr = merge_sort(arr[slice(mid, len(arr))])
+
+    return merge_arr(left_arr, right_arr)
+
+print(merge_sort([1, 10, 30, 50, 2]))
+# print(merge_sort([32, 29, 15, 7]))
+# print(merge_sort([32, 1, 2, 3]))
+# print(merge_sort([32, 1, 15, 2, 3]))
+# print(merge_sort([1, 2, 3, 4, 5]))
